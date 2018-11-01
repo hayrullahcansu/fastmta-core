8994445c7506d9255434362cad51e37269f8d316
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-11-02 00:34:33 +0300

    gitignore

--- entity/model.go
+++ entity/model.go
@@ -1,9 +1,27 @@
 package entity
 
-import "github.com/jinzhu/gorm"
+import (
+	"database/sql"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Product struct {
 	gorm.Model
 	Code  string
 	Price uint
+}
+
+type User struct {
+	gorm.Model
+	Name         string
+	Age          sql.NullInt64
+	Birthday     *time.Time
+	Email        string  `gorm:"type:varchar(100);unique_index"`
+	Role         string  `gorm:"size:255"`        // set field size to 255
+	MemberNumber *string `gorm:"unique;not null"` // set member number to unique and not null
+	Num          int     `gorm:"AUTO_INCREMENT"`  // set num to auto incrementable
+	Address      string  `gorm:"index:addr"`      // create index with name `addr` for address
+	IgnoreMe     int     `gorm:"-"`               // ignore this field
 }
