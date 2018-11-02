7a41b54711c38d65a1c43e11baeea9f36a9b8768
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-11-02 23:45:45 +0300

    Remove temp model

--- entity/model.go
+++ entity/model.go
@@ -1,7 +1,6 @@
 package entity
 
 import (
-	"database/sql"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -23,17 +22,4 @@ type Transaction struct {
 	ServerHostname    string     `gorm:"type:varchar(250);not null;Column:server_hostname"`
 	ServerResponse    string     `gorm:"Column:server_response"`
 	TransactionStatus int        `gorm:"Column:transaction_status"`
-}
-
-type User struct {
-	gorm.Model
-	Name         string
-	Age          sql.NullInt64
-	Birthday     *time.Time
-	Email        string  `gorm:"type:varchar(100);unique_index;"`
-	Role         string  `gorm:"size:255"`        // set field size to 255
-	MemberNumber *string `gorm:"unique;not null"` // set member number to unique and not null
-	Num          int     `gorm:"AUTO_INCREMENT"`  // set num to auto incrementable
-	Address      string  `gorm:"index:addr"`      // create index with name `addr` for address
-	IgnoreMe     int     `gorm:"-"`               // ignore this field
 }
