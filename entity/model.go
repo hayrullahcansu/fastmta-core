3d1b4c84a5c70b9fd748c46fe34727697ba1c257
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-11-02 23:40:38 +0300

    Transaction model implemented

--- entity/model.go
+++ entity/model.go
@@ -9,11 +9,20 @@ import (
 
 type Message struct {
 	gorm.Model
-	ID       string `gorm:"UNIQUE_INDEX;not null;Column:id"`
-	RcptTo   string `gorm:"type:varchar(200);not null;Column:rcpt_to"`
-	MailFrom string `gorm:"type:varchar(250);not null;Column:mail_from"`
-	Data     string `gorm:"not null;Column:data"`
-	Status   string `gorm:"type:varchar(10);not null;Column:status;INDEX"`
+	MessageID string `gorm:"UNIQUE_INDEX;not null;Column:message_id"`
+	RcptTo    string `gorm:"type:varchar(200);not null;Column:rcpt_to"`
+	MailFrom  string `gorm:"type:varchar(250);not null;Column:mail_from"`
+	Data      string `gorm:"not null;Column:data"`
+	Status    string `gorm:"type:varchar(10);not null;Column:status;INDEX"`
+}
+
+type Transaction struct {
+	gorm.Model
+	MessageID         string `gorm:"UNIQUE_INDEX;not null;Column:message_id"`
+	Timestamp         int    `gorm:"Column:transaction_timestamp;INDEX"`
+	ServerHostname    string `gorm:"type:varchar(250);not null;Column:server_hostname"`
+	ServerResponse    string `gorm:"Column:server_response"`
+	TransactionStatus int    `gorm:"Column:transaction_status"`
 }
 
 type User struct {
