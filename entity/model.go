abcbd3c8daf0501ce9de87adedc69897ec46ba26
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-11-04 04:28:38 +0300

    model updated

--- entity/model.go
+++ entity/model.go
@@ -6,14 +6,27 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+type MessageTransaction struct {
+	gorm.Model
+	MessageID          string
+	Headers            map[string]*string
+	RcptTo             []string
+	MailFrom           string
+	Data               string
+	Status             string
+	MimeMode           string
+	MessageDestination string
+}
+
 type Message struct {
 	gorm.Model
-	MessageID string `gorm:"UNIQUE_INDEX;not null;Column:message_id"`
-	RcptTo    string `gorm:"type:varchar(200);not null;Column:rcpt_to"`
-	MailFrom  string `gorm:"type:varchar(250);not null;Column:mail_from"`
-	Data      string `gorm:"not null;Column:data"`
-	Status    string `gorm:"type:varchar(10);not null;Column:status;INDEX"`
-	MimeMode  string `gorm:"type:varchar(10);not null;Column:mimemod"`
+	MessageID          string `gorm:"UNIQUE_INDEX;not null;Column:message_id"`
+	RcptTo             string `gorm:"type:varchar(200);not null;Column:rcpt_to"`
+	MailFrom           string `gorm:"type:varchar(250);not null;Column:mail_from"`
+	Data               string `gorm:"not null;Column:data"`
+	Status             string `gorm:"type:varchar(10);not null;Column:status;INDEX"`
+	MimeMode           string `gorm:"type:varchar(10);not null;Column:mimemod"`
+	MessageDestination string `gorm:"type:varchar(10);not null;Column:message_destination"`
 }
 
 type Transaction struct {
