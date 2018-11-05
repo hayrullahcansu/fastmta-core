a492a476ceeab3f8eda012ef026b6bfd2b59f0d0
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-11-05 23:25:14 +0300

    inboundrabbitmqclient removed from boss

--- boss/Boss.go
+++ boss/Boss.go
@@ -3,14 +3,12 @@ package boss
 import (
 	"../conf"
 	ZMSmtp "../core/smtp"
-	"../queue"
 )
 
 type Boss struct {
-	Config                *conf.Config
-	VirtualMtas           []*ZMSmtp.VirtualMta
-	InboundMtas           []*ZMSmtp.SmtpServer
-	InboundRabbitMqClient *queue.RabbitMqClient
+	Config      *conf.Config
+	VirtualMtas []*ZMSmtp.VirtualMta
+	InboundMtas []*ZMSmtp.SmtpServer
 }
 
 func New(config *conf.Config) *Boss {
@@ -19,20 +17,14 @@ func New(config *conf.Config) *Boss {
 		VirtualMtas: make([]*ZMSmtp.VirtualMta, 0),
 		InboundMtas: make([]*ZMSmtp.SmtpServer, 0),
 	}
-	client := queue.New()
-	client.Connect(config, true)
-	client.ExchangeDeclare(queue.InboundExchange, true, false, false, false, nil)
-	que, err := client.QueueDeclare(queue.InboundStagingQueueName, true, false, false, false, nil)
-	client.QueueBind(que.Name, queue.InboundExchange, "", false, nil)
-	boss.InboundRabbitMqClient = client
-	return
+	return boss
 }
 
 func (boss *Boss) Run() {
 	for _, vmta := range boss.Config.IPAddresses {
 		vm := ZMSmtp.CreateNewVirtualMta(vmta.IP, "vmta1.localhost", 25, vmta.Inbound, vmta.Outbound)
 		boss.VirtualMtas = append(boss.VirtualMtas, vm)
-		inboundServer := ZMSmtp.CreateNewSmtpServer(vm)
+		inboundServer := ZMSmtp.CreateNewSmtpServer(vm, boss.Config)
 		boss.InboundMtas = append(boss.InboundMtas, inboundServer)
 		//TODO: pass InboundRabbitMqClient to inboundServer and inboundHandler
 		go inboundServer.Run()
