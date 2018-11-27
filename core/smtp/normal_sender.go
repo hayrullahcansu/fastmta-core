65b553a4b7091c93b05001d81579bfb0ac515021
hayrullahcansu <hayrullah.cansu@gmail.com>
2018-11-28 01:08:44 +0300

    normal sender implemented

---
+++ core/smtp/normal_sender.go
@@ -0,0 +1,44 @@
+package smtp
+
+import (
+	".."
+	"../../entity"
+)
+
+type NormalSender struct {
+	MessageChannel chan *entity.Message
+	ParentRouter   *Router
+	virtualMta     *VirtualMta
+}
+
+func NewGeneralSender(router *Router) *NormalSender {
+	return &NormalSender{
+		MessageChannel: make(chan *entity.Message, 100),
+		ParentRouter:   router,
+	}
+}
+
+func (sender *NormalSender) AssignVirtualMta() {
+	mta, ok := sender.ParentRouter.GetVirtualMta()
+	if ok {
+		sender.virtualMta = mta
+	}
+}
+
+func (sender *NormalSender) Run() {
+	for {
+		select {
+		case msg, ok := <-sender.MessageChannel:
+			if ok {
+				_, err := core.NewDomain(msg.Host)
+				if err != nil {
+					//TODO: this is bounce domain not found
+					_ = NewOutboundClient()
+					//transactionResult := client.SendMessage(msg, nil, domain)
+					//fmt.Println(transactionResult)
+				}
+
+			}
+		}
+	}
+}
